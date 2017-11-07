0059a954ad00cb7cf43ad065a5b8d689e0d0a5d8
Macc Liu <maccliu@qq.com>
2017-11-07 12:17:10 +0800

    同上

--- tests/db.config.php
+++ tests/db.config.php
@@ -4,17 +4,20 @@
  * http://dida.zeupin.com
  */
 return [
-    'db.dsn'            => 'mysql:host=localhost;port=3306;dbname=zeupin',
-    'db.username'       => 'zeupin',
-    'db.password'       => 'zeupin',
-    'db.options'        => [
+    /* Db */
+    'db.dsn'         => 'mysql:host=localhost;port=3306;dbname=zeupin',
+    'db.username'    => 'zeupin',
+    'db.password'    => 'zeupin',
+    'db.options'     => [
         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_PERSISTENT         => false
     ],
-    'db.schemainfo_dir' => __DIR__ . '/cache',
-    'db.name'           => 'zeupin',
-    'db.type'           => 'mysql',
-    'db.prefix'         => 'zp_',
-    'db.swap_prefix'    => '###_',
+    'db.name'        => 'zeupin',
+    'db.type'        => 'mysql',
+    'db.prefix'      => 'zp_',
+    'db.swap_prefix' => '###_', //
+
+    /* SchemaInfo */
+    'db.schemainfo.cachedir' => __DIR__ . '/cache',
 ];
