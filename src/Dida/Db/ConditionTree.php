935bbb4b7eafedc77df73727970bf2f1f8920415
Macc Liu <maccliu@qq.com>
2017-11-13 18:38:24 +0800

    更新文件版权声明

--- src/Dida/Db/ConditionTree.php
+++ src/Dida/Db/ConditionTree.php
@@ -4,7 +4,7 @@
  * Copyright (c) Zeupin LLC. (http://zeupin.com)
  *
  * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
+ * Redistributions of files MUST retain the above copyright notice.
  */
 
 namespace Dida\Db;
