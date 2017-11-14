0baa453e52dfff618d8df786fc1d58d4980d5229
Macc Liu <maccliu@qq.com>
2017-11-14 21:10:46 +0800

    dev

--- src/Dida/Db/Util.php
+++ src/Dida/Db/Util.php
@@ -19,16 +19,17 @@ class Util
      */
     const VERSION = '20171113';
 
+
     /**
      * 将一个数组按照给出的 key1,key2,keyN 进行键值化，返回处理后的数组。
      *
      * 注意：
-     * 1. 考虑到处理超大数组时的内存占用问题，原数组的数据被逐条处理后，会被删除。
+     * 1. 考虑到处理超大数组时的内存占用问题，原数组的数据会逐条处理，如果此条处理成功，则会被从源数组中删除。
      * 2. 需要自行保证给出的 key1,key2,keyN 的组合可以唯一确定一条记录。
      *    否则，对于同一 key1,key2,keyN，后值将覆盖前值。
      * 3. key1,key2,keyN 一般使用数据表的唯一主键、复合主键、或者有唯一值的字段名。
      *
-     * @param array $array
+     * @param array $array   源数组
      * @param string|int $keyN   要分组的键名或键序号
      *
      * @return array|false   成功返回数组，有错返回false
@@ -83,9 +84,9 @@ class Util
      * 将一个数组按照给出的key进行Group处理，返回Group后的数组。
      *
      * 注意：
-     * 1. 考虑到处理超大数组时的内存占用问题，原数组的数据被逐条处理后，会被删除。
+     * 1. 考虑到处理超大数组时的内存占用问题，原数组的数据会逐条处理，如果此条处理成功，则会被从源数组中删除。
      *
-     * @param array $array
+     * @param array $array   源数组
      * @param string|int $keyN   要分组的键名或键序号
      *
      * @return array|false   成功返回数组，有错返回false
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,10 +3,10 @@
  * Dida Framework --Powered by Zeupin LLC
  * http://dida.zeupin.com
  */
-
-require('D:/Projects/github/dida-autoloader--dev/src/Autoloader.php');
-require('D:/Projects/github/dida-debug--dev/src/Debug/Debug.php');
+require('D:/Projects/github/dida-autoloader--dev/src/Dida/Autoloader.php');
 \Dida\Autoloader::init();
 \Dida\Autoloader::addPsr4('Dida\\', 'D:/Projects/github/dida-db--dev/src/Dida');
+
+require('D:/Projects/github/dida-debug--dev/src/Dida/Debug/Debug.php');
 
 require('D:/Projects/github/composer/vendor/autoload.php');
